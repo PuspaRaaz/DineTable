4095be41888d8d0a31ee8940eb9ba5f6f9ad4a46
पुष्प राज <social@pusparaaz.com.np>
2015-01-24 20:57:21 +0545

    key function added

--- Practice/perspective.cpp
+++ Practice/perspective.cpp
@@ -24,6 +24,14 @@ vertex rotation3D(vertex v, float theta = 1)
 	return res;
 }
 
+vertex translate3D(vertex v, vertex translation){
+	vertex res;
+	res.x = v.x + translation.x;
+	res.y = v.y + translation.y;
+	res.z = v.z + translation.z;
+	return res;
+}
+
 vertex scaling3D(vertex v, float scaleFactor = 1){
 	vertex res;
 	res.x = roundOff(v.x*scaleFactor);
@@ -60,18 +68,9 @@ int main(int argc, char const *argv[])
         {
             switch(event.type)
             {
-                case SDL_QUIT:
-                    quit = true;
-                	break;
                 case SDL_KEYDOWN:
 		        	switch (event.key.keysym.sym)
 		        	{
-		        		case SDLK_ESCAPE:
-		        			quit = true;
-		        			break;
-		        		case SDLK_BACKSPACE:
-		        			quit = true;
-		        			break;
 		        		case SDLK_UP:
 							aT = scaling3D(aT, pivot, 1.1); bT = scaling3D(bT, pivot, 1.1);
 							cT = scaling3D(cT, pivot, 1.1); dT = scaling3D(dT, pivot, 1.1);
@@ -80,6 +79,14 @@ int main(int argc, char const *argv[])
 							aT = scaling3D(aT, pivot, 0.9); bT = scaling3D(bT, pivot, 0.9);
 							cT = scaling3D(cT, pivot, 0.9); dT = scaling3D(dT, pivot, 0.9);
 		        			break;
+		        		case SDLK_LEFT:
+							aT = translate3D(aT, vertex(-5,0,0)); bT = translate3D(bT, vertex(-5,0,0));
+							cT = translate3D(cT, vertex(-5,0,0)); dT = translate3D(dT, vertex(-5,0,0));
+							break;
+						case SDLK_RIGHT:
+							aT = translate3D(aT, vertex(5,0,0)); bT = translate3D(bT, vertex(5,0,0));
+							cT = translate3D(cT, vertex(5,0,0)); dT = translate3D(dT, vertex(5,0,0));
+							break;
 		        		case SDLK_SPACE:
 		        			pause = (pause)?false:true;
 		        			while(pause){
@@ -100,7 +107,25 @@ int main(int argc, char const *argv[])
 		        			break;
 		        	}
 	                break;
+	            case SDL_MOUSEBUTTONUP:
+	            	switch(event.button.button){
+	            		case SDL_BUTTON_LEFT:
+							aT = scaling3D(aT, pivot, 1.01); bT = scaling3D(bT, pivot, 1.01);
+							cT = scaling3D(cT, pivot, 1.01); dT = scaling3D(dT, pivot, 1.01);
+		        			break;
+		        		case SDL_BUTTON_RIGHT:
+							aT = scaling3D(aT, pivot, 0.9); bT = scaling3D(bT, pivot, 0.9);
+							cT = scaling3D(cT, pivot, 0.9); dT = scaling3D(dT, pivot, 0.9);
+		        			break;	            			
+	            	}
+	            	break;
             }
+        }
+        //Get the keystates
+        Uint8 *keystates = SDL_GetKeyState( NULL );
+        //If cancel x or 'q' is pressed 
+        if( keystates[SDL_QUIT] || keystates[SDLK_q]) {
+        	quit = true;
         }
 		aT = rotation3D(aT, 0.2); bT = rotation3D(bT, 0.2);
 		cT = rotation3D(cT, 0.2); dT = rotation3D(dT, 0.2);
@@ -108,9 +133,9 @@ int main(int argc, char const *argv[])
 	    drawLineDDA(screen,aT,bT);
 	    drawLineDDA(screen,bT,cT);
 	    drawLineDDA(screen,cT,aT);
-	    drawLineDDA(screen,aT,dT);
-	    drawLineDDA(screen,bT,dT,255,0,0);
-	    drawLineDDA(screen,cT,dT,0,255,0);
+	    drawLineDDA(screen,aT,dT,255,0,0);
+	    drawLineDDA(screen,bT,dT,0,255,0);
+	    drawLineDDA(screen,cT,dT,0,0,255);
 	    SDL_Flip(screen);
 		SDL_FreeSurface(screen);
     }
