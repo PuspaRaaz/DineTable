c2d8fffd1cc3bceb6a31710b5f4e64f321b7f9ab
पुष्प राज <social@pusparaaz.com.np>
2015-01-30 17:25:40 +0545

    floodfill edited

--- Source/main.cpp
+++ Source/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	while(!quit){
 		SDL_Surface* window = NULL;
 		window = SDL_SetVideoMode(1300,700,32,SDL_RESIZABLE);
-		floodFill(window, 0+window->w/2, 299+window->h/2, 255, 255, 0);
+		floodFill(window, 0+window->w/2, 279+window->h/2, 255, 255, 0);
 		tetrahedron.drawPolygon(window);
 		while(SDL_PollEvent(&event)){
 			switch(event.type){
